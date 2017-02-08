7b1713a33066b71113a1c3d8d50e179f1d5309e1
TaHuC <TaHuC@hotmail.com>
2017-02-08 23:08:23 +0200

    Finish this BIG BIG BIG fac....

---
+++ Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Numerics;
+
+namespace BigFacturial
+{
+	public class MainClass
+	{
+		public static void Main()
+		{
+			var number = int.Parse(Console.ReadLine());
+
+			BigInteger bigNum = 1;
+
+			for (var i = 2; i <= number; i++)
+			{
+				bigNum *= i;
+			}
+
+			Console.WriteLine(bigNum);
+		}
+	}
+}
